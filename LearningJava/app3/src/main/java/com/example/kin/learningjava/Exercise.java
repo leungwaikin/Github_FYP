87ee6a3c992a828428db6278a7c3b2a4bd5c93a3
steveysh <32231243+steveysh@users.noreply.github.com>
2018-02-10 17:23:54 +0800

    merge compiler

---
+++ LearningJava/app3/src/main/java/com/example/kin/learningjava/Exercise.java
@@ -0,0 +1,33 @@
+package com.example.kin.learningjava;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class Exercise extends AppCompatActivity {
+    WebView mwebview;
+    String name;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise);
+        Bundle bundle=getIntent().getExtras();
+        name=bundle.getString("username");
+        mwebview = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = mwebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mwebview.getSettings().setBuiltInZoomControls(true);
+        mwebview.setInitialScale(1);
+        mwebview.loadUrl("https://www.compilejava.net");
+        mwebview.getSettings().setUseWideViewPort(true);
+        mwebview.getSettings().setLoadWithOverviewMode(true);
+        /*Intent in;
+        in = new Intent(Exercise.this,DialogNewClass.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("username",name);
+        in.putExtras(bundle);
+        startActivity(in);*/
+    }
+}
