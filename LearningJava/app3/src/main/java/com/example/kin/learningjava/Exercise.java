531ee5e1bc3d804bcb694ae7c853627aa657edff
steveysh <32231243+steveysh@users.noreply.github.com>
2018-03-13 23:50:53 +0800

    fill in the blank

--- LearningJava/app3/src/main/java/com/example/kin/learningjava/Exercise.java
+++ LearningJava/app3/src/main/java/com/example/kin/learningjava/Exercise.java
@@ -6,14 +6,20 @@ import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.Picture;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.view.View;
 import android.widget.AdapterView;
@@ -57,7 +63,7 @@ public class Exercise extends AppCompatActivity {
         categories.add("Q2.2: According to above question , when the input is higher than 5 , print  \"too large \" ,otherwise print  \"too small \" ");
         categories.add("Q2.3: Using else-if statement to evalute the smallest value of two numbers");
         categories.add("Q2.4: Using NOT operator to evalute a variable is not larger than 10");
-        categories.add("Q2.5:");
+        categories.add("Q2.5: Find the output of the program");
         categories.add("Q2.6:");
         categories.add("Q2.7:Using for loop to print out 1 to 10");
         categories.add("Q2.8:Modify the answer of previous question to print out 1 to 10 using do-while loop (Hints: Using Increment inside the loop");
@@ -132,19 +138,19 @@ public class Exercise extends AppCompatActivity {
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(context);
                 TextView title = new TextView(context);
-                if(spinner.getSelectedItem().toString().length()>50&&spinner.getSelectedItem().toString().length()<=100) {
+                if (spinner.getSelectedItem().toString().length() > 50 && spinner.getSelectedItem().toString().length() <= 100) {
                     title.setTextSize(15);
-                }else if(spinner.getSelectedItem().toString().length()<=50){
+                } else if (spinner.getSelectedItem().toString().length() <= 50) {
                     title.setTextSize(20);
-                }else if(spinner.getSelectedItem().toString().length()>100){
+                } else if (spinner.getSelectedItem().toString().length() > 100) {
                     title.setTextSize(15);
                 }
-                title.setText(spinner.getSelectedItem().toString()+".");
-                title.setPadding(5,5,0,0);
+                title.setText(spinner.getSelectedItem().toString() + ".");
+                title.setPadding(5, 5, 0, 0);
                 title.setTextColor(-16777216);
 // 2. Chain together various setter methods to set the dialog characteristics
 
-               builder.setView(R.layout.exercisequestion);
+                builder.setView(R.layout.exercisequestion);
                 builder.setTitle(spinner.getSelectedItem().toString());
                 builder.setCustomTitle(title);
               /*  if(spinner.getSelectedItemPosition()==14){
@@ -154,16 +160,81 @@ public class Exercise extends AppCompatActivity {
                 }*/
 
 
-
 // 3. Get the AlertDialog from create()
-                final AlertDialog dialog = builder.create();
-                dialog.show();
-               final TextView text = (TextView) dialog.findViewById(R.id.textView15);
-                String[] exercises = context.getResources().getStringArray(R.array.exercises);
-               final  Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
-               final  Button dialogButton2 = (Button) dialog.findViewById(R.id.dialogButtonCopy);
-               final Button dialogButton3 = (Button) dialog.findViewById(R.id.dialogButtonShow);
+                if (spinner.getSelectedItemPosition()==13||spinner.getSelectedItemPosition()==14||spinner.getSelectedItemPosition()==18||spinner.getSelectedItemPosition()==20||spinner.getSelectedItemPosition()==22||spinner.getSelectedItemPosition()==23||spinner.getSelectedItemPosition()==24||spinner.getSelectedItemPosition()==25||spinner.getSelectedItemPosition()==26) {
+                    AlertDialog.Builder builder2 = new AlertDialog.Builder(context);
+                    builder2.setView(R.layout.exercisequestion2);
+                    builder2.setTitle(spinner.getSelectedItem().toString());
+                    builder2.setCustomTitle(title);
+                    final AlertDialog dialog2 = builder2.create();
 
+                    dialog2.show();
+                    String[] exercises = context.getResources().getStringArray(R.array.exercises);
+                    final TextView text2 = (TextView) dialog2.findViewById(R.id.textView17);
+
+                    for (int i = 0; i <= exercises.length; i++) {
+                        if (spinner.getSelectedItemPosition() == 13 || spinner.getSelectedItemPosition() == 14 || spinner.getSelectedItemPosition() == 18 || spinner.getSelectedItemPosition() == 20 || spinner.getSelectedItemPosition() == 22 || spinner.getSelectedItemPosition() == 23 || spinner.getSelectedItemPosition() == 24 || spinner.getSelectedItemPosition() == 25 || spinner.getSelectedItemPosition() == 26) {
+                            if (spinner.getSelectedItemPosition() == i) {
+                                        text2.setText(exercises[i]);
+                                        text2.setTextSize(22);
+
+                            }
+                        }
+                    }
+
+
+                    final Button dialogButton11 = (Button) dialog2.findViewById(R.id.dialogButtonSubmit);
+                    final EditText input1 = (EditText) dialog2.findViewById(R.id.editText5);
+                    dialogButton11.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String input11 = input1.getText().toString();
+                            String ans11=context.getResources().getString(R.string.Q2_5);
+
+                            text2.setText(text2.getText().toString().replace("______",ans11));
+                            dialogButton11.setEnabled(false);
+                            if(input11.equals(ans11)) {
+                               /* Spannable ans = new SpannableString(ans11);
+                                SpannableStringBuilder builder = new SpannableStringBuilder();
+
+                                ans.setSpan(new ForegroundColorSpan(Color.RED), 0, ans.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                builder.append(ans);*/
+
+                                input1.setText(input11+"(Correct)");
+                                input1.setTextColor(Color.rgb(0,255,0));
+                            }
+                            if(!input11.equals(ans11)) {
+                               /* Spannable ans = new SpannableString(ans11);
+                                SpannableStringBuilder builder = new SpannableStringBuilder();
+
+                                ans.setSpan(new ForegroundColorSpan(Color.RED), 0, ans.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                                builder.append(ans);*/
+
+                                input1.setText(input11+"(Incorrect)");
+                                input1.setTextColor(Color.rgb(255,0,0));
+                            }
+                        }
+                    });
+
+
+
+
+                }
+                else {
+                    final AlertDialog dialog = builder.create();
+                    dialog.show();
+                    final TextView text = (TextView) dialog.findViewById(R.id.textView15);
+                    String[] exercises = context.getResources().getStringArray(R.array.exercises);
+                    final Button dialogButton = (Button) dialog.findViewById(R.id.dialogButtonOK);
+                    final Button dialogButton2 = (Button) dialog.findViewById(R.id.dialogButtonCopy);
+                    final Button dialogButton3 = (Button) dialog.findViewById(R.id.dialogButtonShow);
+
+
+                /*final TextView text2 = (TextView) dialog.findViewById(R.id.textView17);
+               final Button dialogButton11 =  (Button) dialog.findViewById(R.id.dialogButtonSubmit);
+               final EditText input1 = (EditText) dialog.findViewById(R.id.editText5);*/
+
+                    dialogButton2.setEnabled(false);
                    /* for(int i=0;i<=exercises.length;i++){
               if(spinner.getSelectedItemPosition()==i) {
                   if(!exercises[i].equals("")) {
@@ -179,56 +250,58 @@ public class Exercise extends AppCompatActivity {
               }
               }*/
 
-                // if button is clicked, close the custom dialog
-               dialogButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        dialog.dismiss();
-                    }
-                });
+                    // if button is clicked, close the custom dialog
+                    dialogButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            dialog.dismiss();
+                        }
+                    });
 
 
-                dialogButton2.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-                        String getstring = text.getText().toString();
-                        android.content.ClipData clip = android.content.ClipData
-                                .newPlainText("Your OTP", getstring);
-                        clipboard.setPrimaryClip(clip);
-                        dialog.dismiss();
-                        Toast.makeText(context,"Answer is copied",
-                                Toast.LENGTH_SHORT).show();
-                    }
-                });
-                //btn3
-                dialogButton3.setOnClickListener(new View.OnClickListener() {
-                    String[] exercises = context.getResources().getStringArray(R.array.exercises);
-                    @Override
-                    public void onClick(View v) {
-                        for(int i=0;i<=exercises.length;i++){
-                            if(spinner.getSelectedItemPosition()==i) {
-                                if(!exercises[i].equals("")) {
-                                    if(text.getText().equals("")) {
-                                        text.setText(exercises[i]);
-                                        dialogButton2.setEnabled(true);
+                    dialogButton2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+                            String getstring = text.getText().toString();
+                            android.content.ClipData clip = android.content.ClipData
+                                    .newPlainText("Your OTP", getstring);
+                            clipboard.setPrimaryClip(clip);
+                            dialog.dismiss();
+                            Toast.makeText(context, "Answer is copied",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    //btn3
+                    dialogButton3.setOnClickListener(new View.OnClickListener() {
+                        String[] exercises = context.getResources().getStringArray(R.array.exercises);
+
+                        @Override
+                        public void onClick(View v) {
+                            for (int i = 0; i <= exercises.length; i++) {
+                                if (spinner.getSelectedItemPosition() == i) {
+                                    if (!exercises[i].equals("")) {
+                                        if (text.getText().equals("")) {
+                                            text.setText(exercises[i]);
+
+                                            dialogButton2.setEnabled(true);
+                                            dialogButton3.setEnabled(false);
+                                        }
+                                    } else if (exercises[i].equals("")) {
+                                        text.setText("(No answer)");
+                                        dialogButton2.setEnabled(false);
+                                    } else if (!text.getText().equals("")) {
+                                        text.setText("");
                                         dialogButton3.setEnabled(false);
                                     }
-                                }
-                                else if(exercises[i].equals("")) {
-                                    text.setText("(No answer)");
-                                    dialogButton2.setEnabled(false);
-                                }
-                                else if(!text.getText().equals("")){
-                                    text.setText("");
-                                    dialogButton3.setEnabled(false);
-                                }
 
 
+                                }
                             }
                         }
-                    }
-                });
+                    });
+                }
+
 
             }
         });
